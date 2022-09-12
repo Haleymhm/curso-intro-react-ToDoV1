cb984189c976c630c0517959d3e0feeeb6cf90e7
Haleym Hidalgo Moyetones <109298016+hahidalgo@users.noreply.github.com>
2022-09-12 13:37:07 -0300

    :contruction: Agregando propiedades

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import './TodoSearch.css';
 
-function TodoSearch() {
+function TodoSearch({ searchValue, setSearchValue })  {
+
     const onSearchValueChange = (event) => {
         console.log(event.target.value);
+        setSearchValue(event.target.value);
     };
 
-    return (
+    return [
         <input
             className="TodoSearch"
-            placeholder=" Buscar Tareas"
+            placeholder="Buscar Tarea"
+            value={searchValue}
             onChange={onSearchValueChange}
-        />
-    );
+        />,
+        <p>{searchValue}</p>
+    ];
 }
 
 export { TodoSearch };
